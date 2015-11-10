37621141b43dd7365d431143c2754f503a1d5f8f
Jonathan Fagan <jfagan@3drobotics.com>
2015-11-09 17:21:54 -0800

    Updated to work with multiple stock locations

--- lib/spree/wombat/handler/set_inventory_handler.rb
+++ lib/spree/wombat/handler/set_inventory_handler.rb
@@ -4,23 +4,27 @@ module Spree
       class SetInventoryHandler < Base
 
         def process
-          stock_location_name = @payload[:inventory][:location]
-
-          stock_location = Spree::StockLocation.find_by_name(stock_location_name) || Spree::StockLocation.find_by_admin_name(stock_location_name)
-          return response("Stock location with name #{stock_location_name} was not found", 500) unless stock_location
-
           sku = @payload[:inventory][:product_id]
           variant = Spree::Variant.find_by_sku(sku)
-          return response("Product with SKU #{sku} was not found", 500) unless variant
+          return response("Product with SKU #{sku} was not found", 404) unless variant
 
-          stock_item = stock_location.stock_items.where(variant: variant).first
+          @payload[:inventory][:locations].each do |inventory_payload|
+            stock_location_name   = inventory_payload.first
+            inventory_at_location = inventory_payload.last
 
-          return response("Stock location '#{stock_location_name}' does not has any stock_items for #{sku}", 500) unless stock_item
+            stock_location = Spree::StockLocation.find_by_name(stock_location_name) || Spree::StockLocation.find_by_admin_name(stock_location_name)
+            return response("Stock location with name #{stock_location_name} was not found", 500) unless stock_location
 
-          count_on_hand = stock_item.count_on_hand
-          stock_item.set_count_on_hand(@payload[:inventory][:quantity])
+            stock_item = stock_location.stock_items.where(variant: variant).first
+            return response("Stock location '#{stock_location_name}' does not has any stock_items for #{sku}", 500) unless stock_item
 
-          return response("Set inventory for #{sku} at #{stock_location_name} from #{count_on_hand} to #{stock_item.reload.count_on_hand}")
+            count_on_hand = stock_item.count_on_hand
+            stock_item.set_count_on_hand(inventory_at_location)
+
+            return response("Set inventory for #{sku} at #{stock_location_name} from #{count_on_hand} to #{stock_item.reload.count_on_hand}")
+
+          end
+
         end
 
       end
