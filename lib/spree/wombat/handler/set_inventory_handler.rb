44cc195444c5f40e94fa0f5d6e56423c4a612985
geermc4 <geermc4@gmail.com>
2016-01-22 14:53:17 -0800

    change response to 200

--- lib/spree/wombat/handler/set_inventory_handler.rb
+++ lib/spree/wombat/handler/set_inventory_handler.rb
@@ -15,7 +15,7 @@ module Spree
 
             inventory_at_location = inventory_payload.last
 
-            return response("Stock location with name #{stock_location_name} was not found", 404) unless stock_location
+            return response("Stock location with name #{stock_location_name} was not found", 200) unless stock_location
 
             stock_item = stock_location.stock_items.where(variant: variant).first
             return response("Stock location '#{stock_location_name}' does not has any stock_items for #{sku}", 500) unless stock_item
